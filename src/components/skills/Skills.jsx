5093d299356f1b0eed50c71a5dd950207ad5f72c
Palacios Tarrillo Christian <85316618+PalaciosTarrilloChristian@users.noreply.github.com>
2022-12-26 21:53:40 -0500

    set padding space

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -7,7 +7,7 @@ import {Backend} from "./Backend";
 export const Skills = () => {
     return (
         <secion className="skills section" id="skills">
-            <h2 className="section__title">Skills</h2>
+            <h2 className="section__title section__title-mod">Skills</h2>
             <span className="section__subtitle">My Technical Level</span>
 
             <div className="skills__container container grid">
