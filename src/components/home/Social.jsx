feb6dff8a41d9e274f0f201e5bd610e6dd02b5ef
Palacios Tarrillo Christian <85316618+PalaciosTarrilloChristian@users.noreply.github.com>
2023-01-07 17:09:57 -0500

    Finished Details

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -6,13 +6,13 @@ export const Social = () => {
 
     return (
         <div className="home__social">
-            <a  rel="noopener" href="https://github.com/PalaciosTarrilloChristian" className="home__social-icon" >
+            <a  target={"_blank"} href="https://github.com/CodigoTotal01" className="home__social-icon" >
                 <i className="uil uil-github"></i>
             </a>
-            <a  rel="noopener" href="https://www.linkedin.com/in/christian-eduardo-palacios-tarrillo-605a78217/" className="home__social-icon" >
+            <a  target={"_blank"} href="https://www.linkedin.com/in/christian-eduardo-palacios-tarrillo-605a78217/" className="home__social-icon" >
                 <i className="uil uil-linkedin"></i>
             </a>
-            <a  rel="noopener" href="https://www.youtube.com/channel/UC2qgmDJD1MacV0LELNCvdCw" className="home__social-icon" >
+            <a  target={"_blank"} href="https://www.youtube.com/channel/UC2qgmDJD1MacV0LELNCvdCw" className="home__social-icon" >
                 <i className="uil uil-youtube"></i>
             </a>
 
