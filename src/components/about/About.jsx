b134e9a46ab580d5750401e3a5e634c8e4cd7f3b
Palacios Tarrillo Christian <85316618+PalaciosTarrilloChristian@users.noreply.github.com>
2023-01-07 23:50:49 -0500

    LAzy Loaded

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,7 +12,7 @@ export const About = () => {
       <h2 className="section__title">About Me</h2>
       <span className="section__subtitle">My information</span>
       <div className="about__container container grid">
-        <img src={AboutImg} alt="" className="about__img" />
+        <img loading="eager" src={AboutImg} alt="" className="about__img" />
         <div className="about__data">
           <Info />
           <p className="about__description">
--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -5,7 +5,7 @@ export const WorkItems = ({item}) =>{
     return(
         <div className="work__card" key={item.id}>
 
-            <img src={item.image} alt="Not found" className="work__img"/>
+            <img loading="lazy" src={item.image} alt="Not found" className="work__img"/>
             <h3 className="work__title">{item.title}</h3>
             <a target="_blank"   href={item.gitUrl} className="work__button">
                 View in GitHub <i className="bx bx-right-arrow-alt work__button-icon"></i>
